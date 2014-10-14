d9fbda693a992ef765a880ce325fda08621afee4
thematthewknot <123321mail@gmail.com>
2014-10-14 17:24:27 -0500

    Added serial input
    basic serial input structure, setup to spit back info for testing.

--- Playground-LED.ino
+++ Playground-LED.ino
@@ -2,8 +2,9 @@
 
 #include <Adafruit_NeoPixel.h>
 
+#define PIN 6 //output to led strip
 
-#define PIN 6
+int incomingByte = 0; // for incoming serial data
 
 void setup() 
 {
@@ -17,7 +18,18 @@ void setup()
 
 void loop() 
 {
+	while(true){
+		if(Serial.available() > 0)
+		{    
+				// read the incoming byte:
+                incomingByte = Serial.read();
 
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+
+		}
+	}
 	
 
 }
