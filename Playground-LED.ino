5d652fd63495f4af74c6cb0ce3c6437e36e5ca5c
thematthewknot <123321mail@gmail.com>
2014-10-19 20:28:18 -0500

    added demo code
    First commit of demo code for DTH demo

--- Playground-LED.ino
+++ Playground-LED.ino
@@ -5,23 +5,46 @@
 #define Lenght 120
 
 int pos = 0;
-int red[Lenght];   // for incoming red color value
-int green[Lenght]; // for incoming green color value
-int blue[Lenght];  // for incoming blue color value
+long red[Lenght];   // for incoming red color value
+long green[Lenght]; // for incoming green color value
+long blue[Lenght];  // for incoming blue color value
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(Lenght, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() 
 {
-  Serial.begin(19200); //setup Serial
+  Serial.begin(9600); //setup Serial
 	strip.begin(); //setup led strip
 	strip.show();
-  Serial.setTimeout(50);
+  Serial.setTimeout(10);
 }
 
 void loop() 
 {
-  	  // if there's any serial available, read it:
+
+ if(Serial.available() ) 
+  {
+      // look for the next valid integer in the incoming serial stream:
+    pos = Serial.parseInt();  // do it again:
+    red[pos] = Serial.parseInt();   // do it again:
+    green[pos] = Serial.parseInt();   // do it again:
+    blue[pos] = Serial.parseInt();   // do it again:
+ 
+  strip.setPixelColor(pos, (int)red[pos], (int)green[pos], (int)blue[pos]); 
+  Serial.print(pos);
+  Serial.print(",");
+  Serial.print(red[pos]);
+  Serial.print(",");
+  Serial.print(green[pos]);
+  Serial.print(",");
+  Serial.println(blue[pos]);
+  }
+
+
+  strip.show();
+}
+
+/* // if there's any serial available, read it:
   if(Serial.available() > 0) 
   {
       // look for the next valid integer in the incoming serial stream:
@@ -29,11 +52,14 @@ void loop()
     red[pos] = Serial.parseInt();   // do it again:
     green[pos] = Serial.parseInt();   // do it again:
     blue[pos] = Serial.parseInt();   // do it again:
+    Serial.print(pos);
+    Serial.print( ",");
+    Serial.println(red[pos]); 
   }
 
   for(int n=1; n < Lenght; n++)
   {
     strip.setPixelColor(n, red[n], green[n], blue[n]); 
   }
-  strip.show();
-}
+  strip.show(); 
+} */
--- dth demo/dthdemo.ino
+++ dth demo/dthdemo.ino
@@ -2,44 +2,40 @@
 
 #define PIN 6
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(120, PIN, NEO_GRB + NEO_KHZ800);
 
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
-
 void setup() {
+  Serial.begin(9600); //setup Serial
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
   // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  colorWipeRev(strip.Color(255, 0, 0), 50); // Red
+ // colorWipe(strip.Color(0, 255, 0), 50); // Green
+ // colorWipe(strip.Color(0, 0, 255), 50); // Blue
   // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127,   0,   0), 50); // Red
-  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+  // theaterChase(strip.Color(127, 127, 127), 50); // White
+  // theaterChase(strip.Color(127,   0,   0), 50); // Red
+  // theaterChase(strip.Color(  0,   0, 127), 50); // Blue
 
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
+  // rainbow(20);
+  // rainbowCycle(20);
+ // theaterChaseRainbow(50);
 }
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
       strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+void colorWipeRev(uint32_t c, uint32_t wait) {
+  for(uint16_t i=strip.numPixels(); i>0; i--) {
+      strip.setPixelColor(i,c);
       strip.show();
       delay(wait);
   }
