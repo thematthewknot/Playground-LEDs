dc511f2cfe9b3a79504551be6980bcca22d0d433
thematthewknot <123321mail@gmail.com>
2014-11-30 01:35:31 -0600

    Cleaned up code
    Okay didn’t really clean anything up, just deleted a bunch YOLO

--- Playground-LED.ino
+++ Playground-LED.ino
@@ -1,5 +1,5 @@
 #include <Adafruit_NeoPixel.h>
-#include <SoftwareSerial.h>
+
 
 #define PIN 5
 #define PIN2 6
@@ -24,17 +24,6 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(120, PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(120, PIN2, NEO_GRB + NEO_KHZ800);
 
 
-
-/*
-const int numReadings = 10;
-
-int readings[numReadings];      // the readings from the analog input
-int index = 0;                  // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
-
-int inputPin = A0;
-*/
 void setup()
 {
   Serial.begin(9600); //setup Serial
@@ -53,9 +42,7 @@ void setup()
 
   // initialize serial communication with computer:
   Serial.begin(9600);                   
-  // initialize all the readings to 0: 
-  //for (int thisReading = 0; thisReading < numReadings; thisReading++)
-  //  readings[thisReading] = 0;          
+        
 }
 
 void loop() {
@@ -80,12 +67,9 @@ if(Serial.available() && state==0)
     if(degree>degreeold){
        jump= ((degree - degreeold)/10) +1;
       for(int i= degreeold; i <= degree; i+=jump){
-        //turnoff(); 
-        
-        //colorSeg(strip.Color(i,0,255-i),  i, 5);
+
         colorSeg(strip.Color(red,green,blue),  i, 5);
-        //delay(1);
-        //delay(1);        // delay in between reads for stability            
+        
       }
   }
 
@@ -93,11 +77,10 @@ if(Serial.available() && state==0)
      jump= ((degreeold -degree)/10) +1;
 
     for(int i= degreeold; i >= degree; i-=jump){
-     // turnoff(); 
+
 
       colorSeg(strip.Color(red,green,blue),  i, 5);
-   //   delay(1);
-      //delay(1);        // delay in between reads for stability            
+       
     }
   }
 
@@ -137,8 +120,9 @@ if(state==1)
     }
     if(infeed==2)
     {Serial.println("starting rainbow");
-      rainbow(20);
+      rainbow(10);
       turnoff();
+      Serial.flush();
      strip.show();
      strip2.show();
       state=0;
@@ -199,13 +183,6 @@ int digitalSmooth(int rawIn, int *sensSmoothArray){     // "int *sensSmoothArray
     }
   }
 
-/*
-  for (j = 0; j < (filterSamples); j++){    // print the array to debug
-    Serial.print(sorted[j]); 
-    Serial.print("   "); 
-  }
-  Serial.println();
-*/
 
   // throw out top and bottom 15% of samples - limit to throw out at least one from top and bottom
   bottom = max(((filterSamples * 15)  / 100), 1); 
@@ -215,13 +192,10 @@ int digitalSmooth(int rawIn, int *sensSmoothArray){     // "int *sensSmoothArray
   for ( j = bottom; j< top; j++){
     total += sorted[j];  // total remaining indices
     k++; 
-    // Serial.print(sorted[j]); 
-    // Serial.print("   "); 
+
   }
 
-//  Serial.println();
-//  Serial.print("average = ");
-//  Serial.println(total/k);
+
   return total / k;    // divide by number of samples
 }
 
@@ -233,10 +207,7 @@ void turnoff() {
   }
   
 
-  //for(uint16_t i=0; i<120; i++) {
-     
-      
- // }
+
  
 }
 
@@ -295,160 +266,3 @@ uint32_t Wheel(byte WheelPos) {
    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   }
 }
-
-
-/*#include <Adafruit_NeoPixel.h>
-#include <SoftwareSerial.h>
-
-#define PIN 5
-#define PIN2 6
-
-SoftwareSerial mySerial(10, 11); // RX, TX
-
-
-
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(120, PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(120, PIN2, NEO_GRB + NEO_KHZ800);
-
-
-void setup() {
-  Serial.begin(9600); //setup Serial
- // Serial.setTimeout(10);
-
-  //Setup Strip 1
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-  strip.setBrightness(255); //Setup global brightness
-
-  //Setup Strip 2
-  strip2.begin();
-  strip2.show(); // Initialize all pixels to 'off'
-  strip2.setBrightness(255); //Setup global brightness
-
-
-  mySerial.begin(9600);
-  
-
-}
-
-void loop() 
-{
-  //byte inputbyte[1];
-char lastdeg;
-  if(Serial.available()) 
-  {
-   
-   uint8_t degree  = Serial.read(); //incoming serial stream
-   degree=(degree/2);
-   Serial.print(degree);
-
-    turnoff(); 
-    colorSeg(strip.Color(degree,0,255-degree),  degree, 5);
-   // lastdeg=degree;
-   //colorSeg(strip.Color(0,0,0),  lastdeg, 3);
-
-   
-  } 
-  
- //if(Serial.available() ) 
- // {
-      // look for the next valid integer in the incoming serial stream:
- //   pos = Serial.parseInt();  // do it again:
- //   red[pos] = Serial.parseInt();   // do it again:
- //   green[pos] = Serial.parseInt();   // do it again:
- //   blue[pos] = Serial.parseInt();   // do it again:
- 
- // strip.setPixelColor(pos, (int)red[pos], (int)green[pos], (int)blue[pos]); 
-
-//colorSeg((0,255,0), 7, 5);
-//colorSeg((0,0,255),  21, 5);
-   
-//colorSeg((0,0,255),  21, 5);
- // }
-//strip.setPixelColor(21, 255,0,0);
-
-  //strip.show();
-//  colorSeg(strip.Color(255,255,0),  21, 5);/
-//delay(250);
- // colorSeg(strip.Color(255,0,0),  2, 35);
-//delay(250);
-
-//delay(250);
-}
-
-
-void turnoff() {
-
-  for(uint16_t i=0; i<120; i++) {
-      strip.setPixelColor(i, strip.Color(0,0,0));
-    
-  }
-  
-
-  for(uint16_t i=0; i<120; i++) {
-      strip2.setPixelColor(i, strip.Color(0,0,0));
-      
-  }
- 
-}
-
-//short segment of moving color
-void colorSeg(uint32_t color, uint8_t location, uint32_t seg) {
-
-  for(uint16_t i=location; i<location+seg; i++) {
-      strip.setPixelColor(i, color);
-    
-  }
-  strip.show();
-  for(uint16_t i=location; i<location+seg; i++) {
-      strip2.setPixelColor(i, color);
-      
-  }
-  strip2.show();
-}
-
-
-
-
-/* 
-
-void colorSeg(uint32_t c, uint8_t location, uint32_t seg) {
-
-  for(uint16_t i=location; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.setPixelColor(i-seg,(0,0,0));
-      strip.show();
-  }
-
-  for(uint16_t i=location; i<strip.numPixels(); i++) {
-      strip2.setPixelColor(i, c);
-      strip2.setPixelColor(i-seg,(0,0,0));
-      strip2.show();
-  }
-}
-
-
-
-
-
-
-// if there's any serial available, read it:
-  if(Serial.available() > 0) 
-  {
-      // look for the next valid integer in the incoming serial stream:
-    pos = Serial.parseInt();  // do it again:
-    red[pos] = Serial.parseInt();   // do it again:
-    green[pos] = Serial.parseInt();   // do it again:
-    blue[pos] = Serial.parseInt();   // do it again:
-    Serial.print(pos);
-    Serial.print( ",");
-    Serial.println(red[pos]); 
-  }
-
-  for(int n=1; n < Lenght; n++)
-  {
-    strip.setPixelColor(n, red[n], green[n], blue[n]); 
-  }
-  strip.show(); 
-} */
